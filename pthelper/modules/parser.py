e35d8463ec8c5e94b4baed33f356b6428562643e
Hamed Khavari <hamed4386.hk@gmail.com>
2026-04-20 23:58:29 +0200

    lakjsdfhlaksjdf

--- pthelper/modules/parser.py
+++ pthelper/modules/parser.py
@@ -5,7 +5,8 @@ def parse_xml(xml_string):
 
     ports = []
     for port in tree.findall('.//port'):
+        service = port.find('service')
+        service_name = service.get('name') if service is not None else 'unknown'
         if port.find('state').get('state') == 'open': 
-            ports.append({'port': port.get('portid'), 'service': port.find('service').get('name')})
+            ports.append({'port': port.get('portid'), 'service': service_name})
     return ports
-            
